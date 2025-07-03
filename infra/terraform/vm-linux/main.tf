9e2104d16fe63e43726f57ed472d9be9dbf01a06
Omar <om276039@gmail.com>
2025-07-04 00:33:08 +0200

    feat: new vm & nsg

--- infra/terraform/vm-linux/main.tf
+++ infra/terraform/vm-linux/main.tf
@@ -12,7 +12,6 @@ resource "azurerm_resource_group" "app_rg" {
   location = var.location
 }
 
-
 resource "azurerm_subnet" "app_subnet" {
   name                 = var.app_vnet_name
   resource_group_name  = data.azurerm_resource_group.hub_rg.name
@@ -20,43 +19,55 @@ resource "azurerm_subnet" "app_subnet" {
   virtual_network_name = data.azurerm_virtual_network.hub.name
 }
 
-
-resource "azurerm_public_ip" "pub_ip" {
-  name                = var.web_public_ip_name
-  location            = var.location
-  resource_group_name = azurerm_resource_group.app_rg.name
-
-  allocation_method = "Static"
+resource "azurerm_subnet" "monitor_subnet" {
+  name                = var.monitor_vnet_name
+  resource_group_name  = data.azurerm_resource_group.hub_rg.name
+  address_prefixes = [var.monitor_address_space]
+  virtual_network_name = data.azurerm_virtual_network.hub.name
 }
 
-resource "azurerm_network_interface" "nic_web" {
-  name                = var.nic_web_name
+resource "azurerm_public_ip" "pub_ip" {
+  for_each            = var.linux_vms
+  name                = each.value.public_ip_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.app_rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "nic" {
+  for_each            = var.linux_vms
+  name                = each.value.nic_name
   location            = azurerm_resource_group.app_rg.location
   resource_group_name = azurerm_resource_group.app_rg.name
 
   ip_configuration {
     name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.app_subnet.id
+    subnet_id = lookup({
+      app     = azurerm_subnet.app_subnet.id,
+      monitor = azurerm_subnet.monitor_subnet.id
+    }, each.value.subnet, azurerm_subnet.app_subnet.id)
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pub_ip.id
+    public_ip_address_id          = azurerm_public_ip.pub_ip[each.key].id
   }
-
 }
 
-
-resource "azurerm_linux_virtual_machine" "web_server" {
-  name                = var.web_vm_name
+resource "azurerm_linux_virtual_machine" "linux_vm" {
+  for_each            = var.linux_vms
+  name                = each.value.vm_name
   location            = azurerm_resource_group.app_rg.location
   resource_group_name = azurerm_resource_group.app_rg.name
   size                = var.web_vm_size
-  admin_username      = var.web_admin_username
+  admin_username      = each.value.admin_username
   network_interface_ids = [
-    azurerm_network_interface.nic_web.id
+    azurerm_network_interface.nic[each.key].id
   ]
 
-  admin_ssh_key {
-    username   = var.web_admin_username
-    public_key = file(var.public_key_path)
+  dynamic "admin_ssh_key" {
+    for_each = each.value.public_key_paths
+    content {
+      username   = each.value.admin_username
+      public_key = file(admin_ssh_key.value)
+    }
   }
 
   os_disk {
@@ -66,15 +77,18 @@ resource "azurerm_linux_virtual_machine" "web_server" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
     version   = "latest"
   }
 }
 
+resource "azurerm_subnet_network_security_group_association" "app_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.app_subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg_app.id
+}
 
-#resource "azurerm_subnet_network_security_group_association" "nic_nsg_assoc" {
-#  subnet_id = azurerm_subnet.app_subnet.id
-#  network_security_group_id = azurerm_network_security_group.web_nsg.id
-#
-#}
+resource "azurerm_subnet_network_security_group_association" "monitor_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.monitor_subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg_monitor.id
+}
--- infra/terraform/vm-linux/terraform.tfvars
+++ infra/terraform/vm-linux/terraform.tfvars
@@ -1,12 +1,28 @@
-hub_rg_name = "rg-hub"
-location = "francecentral"
-app_vnet_name = "vnet-app"
+web_vm_size      = "Standard_B1s"
+hub_rg_name      = "rg-hub"
+location         = "francecentral"
+app_vnet_name    = "vnet-app"
+monitor_vnet_name = "vnet-monitor"
 app_address_space = "10.0.1.0/24"
-web_public_ip_name = "ip-app"
-nic_web_name = "nic-app"
-web_vm_name = "vm-app"
-web_vm_size = "Standard_B1s"  # 4 vCPUs, 16 GiB memory       Standard_D4S_v3
-web_admin_username = "userafricasa"
-public_key_path = "~/.ssh/id_rsa.pub"
-vnet_hub_name = "vnet-hub"
-app_rg_name = "rg-app"
+monitor_address_space = "10.0.2.0/24"
+vnet_hub_name    = "vnet-hub"
+app_rg_name      = "rg-app"
+
+linux_vms = {
+  vm-app = {
+    vm_name          = "vm-app"
+    nic_name         = "nic-app"
+    public_ip_name   = "ip-app"
+    admin_username   = "userafricasa"
+    public_key_paths = ["~/.ssh/id_rsa.pub", "~/.ssh/id_rsa_loic.pub"]
+    subnet= "app"
+  }
+  vm-monitor = {
+    vm_name          = "vm-monitor"
+    nic_name         = "nic-monitor"
+    public_ip_name   = "ip-monitor"
+    admin_username   = "usermonitor"
+    public_key_paths = ["~/.ssh/id_rsa.pub", "~/.ssh/id_rsa_loic.pub"]
+    subnet= "monitor"
+  }
+}
