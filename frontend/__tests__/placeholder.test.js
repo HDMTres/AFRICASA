69a163dd3e49251fadcf6712e3cd82e34f16c4b1
Bali <hadama.toure@supdevinci-edu.fr>
2025-07-04 12:24:45 +0200

    test

---
+++ frontend/__tests__/placeholder.test.js
@@ -0,0 +1,15 @@
+/**
+ * Test placeholder pour AFRICASA Frontend
+ * Ce fichier assure que les tests frontend sont configurés
+ */
+
+describe('AFRICASA Frontend Tests', () => {
+  test('should have a valid test environment', () => {
+    expect(true).toBe(true);
+  });
+  
+  test('should be able to render components', () => {
+    // Test basique pour vérifier que l'environnement de test est configuré
+    expect(typeof React).toBeDefined();
+  });
+});
